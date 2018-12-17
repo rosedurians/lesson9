35e26ed9cdccb68f81b8000bcddd4780edbb1aa2
rosedurians <44170550+rosedurians@users.noreply.github.com>
2018-12-17 11:27:05 -0500

    Add files via upload

---
+++ Lesson 9/problem3.py
@@ -0,0 +1,14 @@
+print('-'*60)
+print('Alien Greeter Bot:')
+print()
+password = input('What planet are you from, ugly? ')
+if password == 'Earth':
+	print('Daaaaaaaaammmmmmmmmmnnnnnnn, your face is so ugly I thought you were an alien.')
+	print('Haha, I am joking. But you ARE ugly though.')
+	print('Have a happy holiday season!')
+else:
+	print('You LIED!!!')
+	print('Prepare to get BLASTED!!!')
+	print('Pew Pew Pew!!!!!')
+print('-'*60)
+
